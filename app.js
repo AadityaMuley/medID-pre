0ddfea984376f10dc47d8b22d38221fa0808aacd
AadityaMuley <aadityamuley8190@gmail.com>
2020-10-27 23:33:19 +0530

    fetch Allergies data from Firestore.

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+const allist = document.querySelector("#allergies");
+
+function renderAl(doc){
+	let li = document.createElement("li");
+	let name = document.createElement("span");
+	let freq = document.createElement("span");
+
+	li.setAttribute("data-id", doc.id);
+	name.textContent = doc.data().name;
+	freq.textContent = doc.data().frequency;
+
+	li.appendChild(name);
+	li.appendChild(freq);
+
+	allist.appendChild(li);
+}
+
 db.collection("allergies").get().then((snapshot) => {
 	snapshot.docs.forEach(doc => {
-		console.log(doc.data())
+		renderAl(doc);
 	})
 })
