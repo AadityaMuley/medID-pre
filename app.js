4062fbdac7ad446f98898163f644efb8babb1b9b
AadityaMuley <aadityamuley8190@gmail.com>
2020-10-28 22:55:21 +0530

    submit form connected to firestore and every user data stored. suggestions changed to jquery

--- app.js
+++ app.js
@@ -1,66 +1,48 @@
-// const htmllist = document.querySelector("#al");
+src="https://code.jquery.com/jquery-1.12.4.js"
+src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"
+
+
+const form = document.querySelector("#user-form");
 var allist = [];
+var namelist = [];
 
-// function renderAl(i){
-// 	let ta = document.createElement("table");
-// 	let row = document.createElement("tr");
-// 	let name = document.createElement("th");
-// 	// let freq = document.createElement("th");
 
-// 	row.setAttribute("data-id", allist[i][0]);
-// 	name.textContent = allist[i][1];
-// 	// freq.textContent = doc.data().frequency;
 
-// 	row.appendChild(name);
-// 	// row.appendChild(freq);
-// 	ta.appendChild(row);
-
-// 	htmllist.appendChild(ta);
-// }
-
+//getting suggestions from firestore database
 db.collection("allergies").get().then((snapshot) => {
 	var i = 0;
 	snapshot.docs.forEach(doc => {
 		var temp = [doc.data().name, doc.data().frequency];
 		allist.push(temp);
-
-		// renderAl(i);
-		// i++;
 	})
 	allist.sort(function(a, b) {
 		return b[1]-a[1];
 	  })
 	console.table(allist);
+	for(var i=0; i<allist.length; i++) {
+		namelist.push(allist[i][0]);
+	}
+	console.log(namelist);
 })
 
+//auto suggestions while giving  input
+$("#allergies").autocomplete({
+	source: namelist
+});
 
 
-function suggest(input, suggestion) {
-	var objip = document.getElementById(input);
-	var strip = objip.value;
 
-	var objsu = document.getElementById(suggestion);
-
-	if(strip.length > 0) {
-		objsu.innerHTML = "";
-		var sulist = document.createElement("ul");
-
-		for (var i=0; i<allist.length; i++) {
-			var allergy = allist[i][0];
-			var alpart = allergy.substring(0,strip.length);
-			if(allergy.length > strip.length && alpart === strip) {
-				var lists = document.createElement("li");
-				lists.setAttribute("onclick", "complete('" + allergy + "', '" + input + "', '" + suggestion + "');");
-                lists.innerHTML = allergy;
-                sulist.appendChild(lists);
-			}
-		}
-
-		objsu.appendChild(sulist);
-	}
-	else {
-		objsu.innerHTML = "";
-	}
-}
-
-// function complete()
+//submit form and send data to firestore
+form.addEventListener("submit", (el)=>{
+	el.preventDefault();
+	db.collection("user").add({
+		name: form.name.value,
+		dob: form.dob.value,
+		gender: form.gen.value,
+		allergy: form.allergies.value
+	});
+	form.name.value = "";
+	form.dob.value = "";
+	form.gen.value = "";
+	form.allergies.value = "";
+})
